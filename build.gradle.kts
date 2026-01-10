597995a7927b66d9fd0f294ddccaad362a77717f
Oscar Chevalier <oscar@frozenmilk.dev>
2026-01-11 09:12:11 +1100

    chore: update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("dev.frozenmilk.teamcode") version "11.0.0-1.0.0"
+    id("dev.frozenmilk.teamcode") version "11.0.0-1.1.0"
     id("dev.frozenmilk.sinister.sloth.load") version "0.2.4"
 }
 
